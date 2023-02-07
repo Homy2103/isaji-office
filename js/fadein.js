d072617d2b3798ef2fdc3124858832ac3d065c61
Homy2103 <124660319+Homy2103@users.noreply.github.com>
2023-02-07 16:41:07 +0900

    Update fadein.js

--- js/fadein.js
+++ js/fadein.js
@@ -1,20 +1,17 @@
-// 動きのきっかけとなるアニメーションの名前を定義
 function fadeAnime(){
 
-  // ふわっ
-  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
-    var elemPos = $(this).offset().top-50;//要素より、50px上の
+  $('.fadeUpTrigger').each(function(){ 
+    var elemPos = $(this).offset().top-50;
     var scroll = $(window).scrollTop();
     var windowHeight = $(window).height();
     if (scroll >= elemPos - windowHeight){
-    $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+    $(this).addClass('fadeUp');
     }else{
-    $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+    $(this).removeClass('fadeUp');
     }
     });
 }
 
-// 画面をスクロールをしたら動かしたい場合の記述
   $(window).scroll(function (){
-    fadeAnime();/* アニメーション用の関数を呼ぶ*/
-  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+    fadeAnime();
+  });
